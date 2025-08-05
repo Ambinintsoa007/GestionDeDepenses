c1b35e1ed778b280675e9735b2551c3baebd89e0
natolotra <natolotra@gmail.com>
2025-08-05 09:53:08 +0200

    feat:ajout du code RecurringExpense

--- GestionDepense/src/main/java/RecurringExpense.java
+++ GestionDepense/src/main/java/RecurringExpense.java
@@ -4,9 +4,18 @@ public class RecurringExpense {
 
     public RecurringExpense(Reccurence recurence) {
         this.recurence = recurence;
+        if (recurence != "monthly" && recurence != "yearly"){
+            return "Recurence is invalid";
+        }
     }
 
     public Reccurence getRecurence() {
         return recurence;
+    }
+
+    @Override
+    public String toString() {
+        return "Refundable Expense: Label = " + getLabel() + ", Amount = $" + String.format("%.2f", getAmount())
+                + ", Date = " + getDate() + ", Refunded = " + refunded;
     }
 }
